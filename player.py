259dd566ddd52d3879186b5a09cfe7854ad9d6fe
ian <ianh@mac.com>
2024-08-28 23:42:32 +0000

    player movement

--- player.py
+++ player.py
@@ -23,11 +23,18 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_w]:
+            self.move(dt)
         if keys[pygame.K_a]:
-            self.rotate(dt * -1) 
+            self.rotate(-dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
 
     def draw(self, screen):
         pygame.draw.polygon(screen, (255, 255, 255, 0) , self.triangle(), 2)
