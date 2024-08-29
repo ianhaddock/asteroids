7de4b0fa7376a0cb7283ae6245b1f355a9dd802d
ian <ianh@mac.com>
2024-08-29 05:22:41 +0000

    shots collision

--- main.py
+++ main.py
@@ -45,6 +45,10 @@ def main():
             if asteroid.collision(player):
                 print('Game over!')
                 raise SystemExit
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.kill()
+                    shot.kill()
 
         #player.update(dt)
 
